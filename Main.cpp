00b8ef8f1e90cef11a7dfc27562dacb949642262
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2024-02-25 19:46:26 +0100

    Modify main

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,16 @@
-// #include "src/Application.h"
-// #include "src/Log.h"
+#include "src/Application.h"
+#include "src/Log.h"
 #include "src/Macro.h"
 
 int main(void) {
   Core::Log::Inizialize();
-  // auto app = Core::Application::GetInstance();
-  // LOG_CORE_INFO("App run");
+  auto app = Core::Application::GetInstance();
+  LOG_CORE_INFO("App run");
 
-  // app->Run();
-  // delete app;
+  app->Run();
+  delete app;
 
-  CORE_ASSERT(false, "ciao!");
+  // CORE_ASSERT(false, "ciao!");
 
   return 0;
 }
