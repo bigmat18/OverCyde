17c6fa8550723a673d84bf96c61350a26fe20503
Dari8987898 <dariuzzolo58@gmail.com>
2024-03-16 00:59:41 +0100

    Created firt window

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -7,9 +7,16 @@ using namespace Engine;
 class Game : public Application {  
     friend class Application;
 
-    private: Game() : Application() {}
+    private: Game(ApplicationProps& props) : Application(props) {}
 };
 
 Application* Application::Create() {
-    return Application::SetInstance(new Game());
+    ApplicationProps props = ApplicationProps();
+
+    props.WProps.Title = "ColziGame";
+    props.WProps.Width = 360;
+    props.WProps.Height = 360;
+    props.BGColor = Vec4f(HEX_COLOR(0xBF00FFFF));
+
+    return Application::SetInstance(new Game(props));
 }
