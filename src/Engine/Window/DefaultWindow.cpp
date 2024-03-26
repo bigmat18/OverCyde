a3c00bf6686a1c8e7ec481cd41efa02fa907afde
Dari8987898 <50867191+Dari8987898@users.noreply.github.com>
2024-03-26 12:45:16 +0100

    Corrected DefaultWindow.cpp

--- src/Engine/Window/DefaultWindow.cpp
+++ src/Engine/Window/DefaultWindow.cpp
@@ -58,68 +58,76 @@ namespace Engine {
             WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
 
             switch (action) {
-				case GLFW_PRESS: {
-                    KeyPressedEvent &event = *(new KeyPressedEvent(key, 0));
-                    data.EventCallback(event);
-					break;
-				}
-				case GLFW_RELEASE: {
-                    KeyReleasedEvent &event = *(new KeyReleasedEvent(key));
-                    data.EventCallback(event);
-					break;
-				}
-				case GLFW_REPEAT: {
-					KeyPressedEvent &event = *(new KeyPressedEvent(key, true));
-					data.EventCallback(event);
-                    break;
+		case GLFW_PRESS: {
+			KeyPressedEvent &event = *(new KeyPressedEvent(key, 0));
+			data.EventCallback(event);
+			break;
+		}
+		    
+		case GLFW_RELEASE: {
+			KeyReleasedEvent &event = *(new KeyReleasedEvent(key));
+			data.EventCallback(event);
+			break;
+		}
+		
+	    	case GLFW_REPEAT: {
+			KeyPressedEvent &event = *(new KeyPressedEvent(key, true));
+			data.EventCallback(event);
+                    	break;
                 }
             }
         });
 
         glfwSetCharCallback(m_Window, [](GLFWwindow *window, unsigned int keycode) {
-			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+		WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
 
-			KeyTypedEvent &event = *(new KeyTypedEvent(keycode));
-			data.EventCallback(event);
+		KeyTypedEvent &event = *(new KeyTypedEvent(keycode));
+		data.EventCallback(event);
         });
 
         glfwSetMouseButtonCallback(m_Window, [](GLFWwindow *window, int button, int action, int mods) {
-			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+		WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
 
-			switch (action) {
-				case GLFW_PRESS: {
-                    MouseButtonPressedEvent &event = *(new MouseButtonPressedEvent(button));
-                    data.EventCallback(event);
-                    break;
-				}
-                case GLFW_RELEASE: {
-                    MouseButtonReleasedEvent &event = *(new MouseButtonReleasedEvent(button));
-                    data.EventCallback(event);
-                    break;
-				}
-			} });
+		switch (action) {
+			case GLFW_PRESS: {
+			    MouseButtonPressedEvent &event = *(new MouseButtonPressedEvent(button));
+			    data.EventCallback(event);
+			    break;
+			}
+			case GLFW_RELEASE: {
+			    MouseButtonReleasedEvent &event = *(new MouseButtonReleasedEvent(button));
+			    data.EventCallback(event);
+			    break;
+			}
+		} 
+	});
 
         glfwSetScrollCallback(m_Window, [](GLFWwindow *window, double xOffset, double yOffset) {
-			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
-
-			MouseScrolledEvent &event = *(new MouseScrolledEvent((float)xOffset, (float)yOffset));
-			data.EventCallback(event); 
+		WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+		
+		MouseScrolledEvent &event = *(new MouseScrolledEvent((float)xOffset, (float)yOffset));
+		data.EventCallback(event);
         });
 
         glfwSetCursorPosCallback(m_Window, [](GLFWwindow *window, double xPos, double yPos) {
-			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
-
-			MouseMovedEvent &event = *(new MouseMovedEvent((float)xPos, (float)yPos));
-			data.EventCallback(event); 
+		WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+		
+		MouseMovedEvent &event = *(new MouseMovedEvent((float)xPos, (float)yPos));
+		data.EventCallback(event);
         });
 
-    	const GLubyte* renderer		= glGetString(GL_RENDERER);
-	    const GLubyte* vendor		= glGetString(GL_VENDOR);
-	    const GLubyte* version		= glGetString(GL_VERSION);
+    	const GLubyte* renderer	= glGetString(GL_RENDERER);
+    	const GLubyte* vendor = glGetString(GL_VENDOR);
+    	const GLubyte* version = glGetString(GL_VERSION);
 
-	    LOG_ENGINE_INFO("GL Vendor            : {0}", reinterpret_cast<const char*>(vendor));
-	    LOG_ENGINE_INFO("GL Renderer          : {0}", reinterpret_cast<const char*>(renderer));
-	    LOG_ENGINE_INFO("GL Version (string)  : {0}", reinterpret_cast<const char*>(version));
+	LOG_ENGINE_INFO("GL Vendor: {0}", reinterpret_cast<const char*>(vendor));
+	LOG_ENGINE_INFO("GL Renderer: {0}", reinterpret_cast<const char*>(renderer));
+	LOG_ENGINE_INFO("GL Version (string): {0}", reinterpret_cast<const char*>(version));
+
+	#ifdef __APPLE__
+            const GLubyte *glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
+            LOG_ENGINE_INFO("GLSL Version         : {0}", reinterpret_cast<const char *>(glslVersion));
+	#endif
     }
 
     void DefaultWindow::Shutdown() {
