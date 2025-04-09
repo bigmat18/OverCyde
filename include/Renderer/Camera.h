8db93bf4e96fc7dbfb6c0f10629fa86f506593b4
bigmat18 <mat.giu2002@gmail.com>
2025-04-09 23:39:19 +0200

    Fix compiling error

--- include/Renderer/Camera.h
+++ include/Renderer/Camera.h
@@ -6,6 +6,7 @@
 namespace Engine {
     class Camera {
         public:
+            Camera() = default;
             Camera(glm::vec3 position, glm::vec3 front, glm::vec3 up, glm::mat4 projection);
 
             const glm::mat4 &GetProjectionMatrix() const { return this->m_ProjectionMatrix; }
@@ -47,6 +48,7 @@ namespace Engine {
             };
 
         public:
+            OrthographicCamera() = default;
             OrthographicCamera(glm::vec3 position, glm::vec3 front, glm::vec3 up, OrthographicData data);
             virtual void RecalculateProjectionMatrix() override;
 
--- include/Renderer/CameraController.h
+++ include/Renderer/CameraController.h
@@ -7,26 +7,27 @@ namespace Engine {
 
     class CameraController {
         public: 
-            virtual void OnUpdate(float deltaTime);
-            virtual void OnEvent(Event& e);
+            virtual void OnUpdate(float deltaTime) = 0;
+            virtual void OnEvent(Event& e) = 0;
 
-            virtual glm::mat4 GetProjectionViewMatrix() const;
+            virtual glm::mat4 GetProjectionViewMatrix() const = 0;
     };
 
     class Camera2DController : public CameraController {
         public:
+            Camera2DController() = default;
             Camera2DController(float aspectRatio, bool rotation = false);
 
             void OnUpdate(float deltaTime) override;
             void OnEvent(Event &e) override;
 
-            glm::mat4 GetProjectionViewMatrix() const override{ 
+            glm::mat4 GetProjectionViewMatrix() const override { 
                 return m_Camera.GetProjectionViewMatrix(); 
             }
 
-        private:
             bool OnMouseScrolled(MouseScrolledEvent& e);
             bool OnWindowResized(WindowResizeEvent& e);
+        private:
 
             float m_CameraTranslationSpeed = 2.0f;
             float m_CameraRotationSpeed = 1.0f;
